a86a3250097d763dfcfb9b04ccb6c4552d5f1877
Masdrii <adribektyawan@gmail.com>
2024-08-08 10:14:00 +0700

    2

--- routes/api.php
+++ routes/api.php
@@ -30,13 +30,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Route::put('kategori/{id}',[KategoriController::class,'update']);
 // Route::delete('kategori/{id}',[KategoriController::class,'destroy']);
 
-Route::middleware(['auth:sanctum'])->group(function() {
+// Route::middleware(['auth:sanctum'])->group(function() {
     Route::post('logout',[AuthController::class, 'logout']);
     Route::resource('kategori', KategoriController::class);
     Route::resource('genre', GenreController::class);
     Route::resource('aktor', AktorController::class);
     Route::resource('film', FilmController::class);
-});
+// });
 
 Route::post('register',[AuthController::class,'register']);
 Route::post('login', [AuthController::class,'login']);
